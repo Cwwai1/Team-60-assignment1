cc5ad898fa03eaffda713ad686bde6c3d649c78b
kscha39 <kscha39@student.monash.edu>
2017-03-10 17:54:27 +1100

    Update server.js

--- server.js
+++ server.js
@@ -1,58 +1,40 @@
-// server.js
 var express        = require('express');  
 var app            = express();  
 var httpServer = require("http").createServer(app);  
 var five = require("johnny-five");  
 var io=require('socket.io')(httpServer);
- 
-var port = 3000; 
- 
-app.use(express.static(__dirname + '/public'));
- 
-app.get('/', function(req, res) {  
-        res.sendFile(__dirname + '/public/index.html');
-});
- 
-httpServer.listen(port);  
-console.log('Server available at http://localhost:' + port);  
 
-//Arduino board connection
- 
-var board = new five.Board(); 
-var led; 
-board.on("ready", function() {  
-    console.log('Arduino connected');
-    led = new five.Led(13);
-    var motion = new five.Motion(2);
+app.get('/', function(req,res){
 
-    motion.on("motionready", function(){
-        console.long('Motion Ready')
-    })
+		//send the index.html file for all requests
+		res.sendFile(__dirname + '/index.html');
+	});
 
-    motion.on("motionstart",function(){
-        console.log('Motion Received');
-    });
+httpServer.listen(3001, function(){
 
-    motion.on("motionend", function(){
-        console.log('Motion Ended');
-    });
+		console.log('listening on http://localhost:3001');
+	});
 
-});
- 
-//Socket connection handler
-io.on('connection', function (socket) {  
-        console.log(socket.id);
- 
-        socket.on('led:on', function (data) {
-           led.on();
-           console.log('LED ON RECEIVED');
-        });
- 
-        socket.on('led:off', function (data) {
-            led.off();
-            console.log('LED OFF RECEIVED');
-            
-        });
-    });
- 
-console.log('Waiting for connection');
+//Connect to arduino
+var board = new five.Board();
+var led;
+board.on('ready', function(){
+		
+		console.log('Arduino connected');
+		led = new five.Led(13);
+	});
+
+
+//socket connection handler
+io.on('connection', function(socket){
+		socket.on('led:on',  function(data){
+			led.on();
+			console.log('Led is on!');
+		});
+
+		socket.on('led:off', function(data){
+			led.off();
+			console.log('Led is off!')
+		});
+	});
+		
